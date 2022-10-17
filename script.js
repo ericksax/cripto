a6077f3466b766ad9c35cae84cd5e66767eeecbf
ERICK FREITAS <erickfreitas@MacBook-Air-de-ERICK.local>
2022-10-17 17:57:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const oculta = document.querySelector('#name')
+
+function toogle() {
+    const name = document.querySelector('.oculta').style.display;
+    name = none ? block: none
+}
+
+oculta.addEventListener('click', toogle())
